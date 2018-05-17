905fc7da4d1751364f9eefa01c770294baa7b91e
cuong <ngmcuongcc@gmail.com>
2018-05-17 20:58:03 +0700

    test

--- test/_getFreePort.js
+++ test/_getFreePort.js
@@ -5,7 +5,7 @@ export default function getFreePort (cb) {
     const server = net.createServer()
     server.listen(() => {
       const port = server.address().port
-      server.close(() => resolve(port))
+      server.close(() => resolve(port));
     })
   })
 }
